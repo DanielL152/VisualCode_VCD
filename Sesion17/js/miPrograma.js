22291e315f9d32839610a95cf62a2afc083a2a65
DanielL152 <72740955@usat.pe>
2021-05-30 12:17:29 -0500

    master

---
+++ Sesion17/js/miPrograma.js
@@ -0,0 +1,19 @@
+
+function guardarDatos(){
+    localStorage.nombre=document.getElementById("nombre").value;
+    localStorage.numero=document.getElementById("numero").value;
+}
+
+
+function recuperarDatos(){
+    if ((localStorage.nombre!=undefined)&&(localStorage.numero!=undefined)){
+
+        document.getElementById("datos").innerHTML="Nombre: "+localStorage.nombre+ "<br> Nº Celular: "+localStorage.numero;
+
+    }else{
+
+        document.getElementById("datos").innerHTML="No existen datos registrados";
+
+    }
+
+}
